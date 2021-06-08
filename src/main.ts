ed65c08f865f5e829b4ac1c088a99417aafd5994
Ahmed Ibrahim <ahmedolaibrahim@gmail.com>
2021-06-08 16:41:18 +0100

    feat: transforms service to microservice, uses nats as transport layer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      url: 'nats://identityevents-dev-nats.energyweb.org:4222',
+    },
+  });
+  app.listen(() => console.log('IamOrgCreator Service is now listening to NATS events..'));
 }
 bootstrap();
