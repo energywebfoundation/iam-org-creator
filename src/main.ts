168594ff6f38b272214e7c33b99117af3ce63b45
Ahmed Ibrahim <ahmedolaibrahim@gmail.com>
2021-06-09 12:59:18 +0100

    chore: setup initial scaffold

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { Transport,MicroserviceOptions } from '@nestjs/microservices';
-import { AppModule } from './module';
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import * as dotenv from 'dotenv';
+import { AppModule } from './module';
+
 dotenv.config();
 
-async function bootstrap() {  
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.NATS,
-    options: {
-      url: process.env.NATS_CLIENTS_URL,
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        url: process.env.NATS_CLIENTS_URL,
+      },
     },
-  });
+  );
 
   const configService = app.get(ConfigService);
-  app.listen(() => console.log(`IamOrgCreator Service is now listening to NATS events on port: ${configService.get('NESTJS_PORT')}`));
+  app.listen(() =>
+    Logger.log(
+      `IamOrgCreator Service is now listening to NATS events on port: ${configService.get(
+        'NESTJS_PORT',
+      )}`,
+    ),
+  );
 }
 
 bootstrap();
