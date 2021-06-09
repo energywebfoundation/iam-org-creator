6670de35990e2c798d0302a52db728a08d3363f4
Ahmed Ibrahim <ahmedolaibrahim@gmail.com>
2021-06-09 10:39:52 +0100

    chore: strip unwanted files

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
-import { AppModule } from './app.module';
+import { AppModule } from './module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.NATS,
     options: {
-      url: 'nats://identityevents-dev-nats.energyweb.org:4222',
+      url: 'nats://volta-identityevents-nats.energyweb.org:4222',
     },
   });
   app.listen(() => console.log('IamOrgCreator Service is now listening to NATS events..'));
